710a9b0493186d5b4b0eb9867a9eeb16865d6e70
Andrii Kulikov <jadecoresky@gmail.com>
2018-08-24 13:17:54 +0300

    Moved all structs to separate file

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,14 @@ extern crate reqwest;
 
 extern crate serde_json;
 extern crate serde;
-
 #[macro_use]
 extern crate serde_derive;
-
-//use serde_json::Error;
-use serde_json::Value;
 
 use std::{thread, time};
 use std::collections::HashMap;
 use std::env;
 
-#[derive (Debug, Serialize, Deserialize)]
-struct TResponse {
-    ok: bool,
-    result: Vec<Value>,
-}
-
-fn parse_response(t_response: &str) -> TResponse {
-    let parsed_response: TResponse = serde_json::from_str(t_response).unwrap();
-    return parsed_response;
-}
+mod parse;
 
 fn get_bot_token() -> String {
     return match env::var("TELEGRAM_BOT_TOKEN") {
@@ -33,6 +20,7 @@ fn get_bot_token() -> String {
 
 fn main() {
     let token: String = get_bot_token();
+    let mut _offset: i32;
     loop {
         let client = reqwest::Client::new();
         let mut map = HashMap::new();
@@ -49,7 +37,7 @@ fn main() {
 
         let text = response_test.text().unwrap();
 
-        let parsed_response: TResponse = parse_response(text.as_str());
+        let parsed_response: parse::TResponse = parse::parse_response(text.as_str()).unwrap();
         println!("{:#?}", parsed_response);
 
         thread::sleep(time::Duration::from_millis(1000));
---
+++ src/parse.rs
@@ -0,0 +1,28 @@
+use serde_json::Error;
+use serde_json::from_str;
+
+#[derive (Debug, Serialize, Deserialize)]
+pub struct TResponse {
+    ok: bool,
+    result: Vec<TResult>,
+}
+
+#[derive (Debug, Serialize, Deserialize)]
+struct TResult {
+    message: TMessage,
+    update_id: i32,
+}
+
+
+#[derive (Debug, Serialize, Deserialize)]
+struct TMessage {
+    date: i32,
+    message_id: i8,
+    text: String,
+}
+
+
+pub fn parse_response(t_response: &str) -> Result<TResponse, Error> {
+    let parsed_response: TResponse = from_str(t_response)?;
+    Ok(parsed_response)
+}
